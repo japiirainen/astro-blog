91b453df2168372c08da7dba89d5c51674b3794f
Joona Piirainen <joona.piirainen@gmail.com>
2021-10-28 16:57:50 +0300

    fix typo in site

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,6 @@ export default /** @type {import('astro').AstroUserConfig} */ ({
   // Enable the Preact renderer to support Preact JSX components.
   renderers: ['@astrojs/renderer-preact'],
   buildOptions: {
-    site: 'https://japiirainen.github.io/
+    site: 'https://japiirainen.github.io/',
   },
 })
